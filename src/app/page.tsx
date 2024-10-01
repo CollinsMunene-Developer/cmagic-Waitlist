0307af72b86533e34a69f7106bca05d60fef5e2c
CollinsMunene-Developer <collinsentrepreneur@gmail.com>
2024-10-01 13:51:52 +0300

    changed ports to 4000

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 import {frame1} from "../../src/app/assets/images/frames/frames"
 import Image from "next/image";
-import {background1} from "./assets/images/Background/background"
-
+import {background1} from "./assets/images/Background/background";
+import {featuredicon3} from "./assets/icons/featuredicons/featuredicons"
 export default function Home() {
   return (
     <div className="">
          <h1>collins is great </h1>
+         <Image src={featuredicon3} alt="featured icons" />
          <Image src={frame1} alt="" />
          <Image src={background1} alt="" />
          <h1>collins is great </h1>
