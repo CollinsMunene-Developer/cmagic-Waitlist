63c4a01e476c6336e83f2e08534cf8de8950f24a
CollinsMunene-Developer <collinsentrepreneur@gmail.com>
2024-10-10 11:36:55 +0300

    "Added spinner component and video playback functionality to Home page"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import {
   azure,
@@ -13,8 +15,32 @@ import "./css/cloudmagic.webflow.css";
 import "./css/normalize.css";
 import "./css/webflow.css";
 
+// Spinner component
+const Spinner = () => (
+  <div className="spinner">
+    <div className="spinner-inner"></div>
+  </div>
+);
+
 export default function Home() {
   const router = useRouter();
+  const [showVideo, setShowVideo] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const videoDetails = {
+    url: "https://youtube.com/watch?v=OcV4RI1UWM0",
+    thumbnailUrl: "https://i.ytimg.com/vi/OcV4RI1UWM0/hqdefault.jpg",
+    src: "https://www.youtube.com/embed/OcV4RI1UWM0?feature=oembed",
+  };
+
+  const handlePlayClick = () => {
+    setIsLoading(true);
+    // Simulate loading time
+    setTimeout(() => {
+      setShowVideo(true);
+      setIsLoading(false);
+    }, 2000);
+  };
 
   const handleJoinWaitlist = () => {
     router.push("/join-waitlist");
@@ -24,12 +50,12 @@ export default function Home() {
     <div className="body">
       <nav className="nav">
         <Link href="/" className="brand-nav w-inline-block">
-          <Image 
-                src={contentlogo}
-                alt=""
-                className="play-imagecmagic logo"
-                width={161}
-                height={35}
+          <Image
+            src={contentlogo}
+            alt=""
+            className="play-imagecmagic logo"
+            width={161}
+            height={35}
           />
         </Link>
       </nav>
@@ -37,22 +63,29 @@ export default function Home() {
         <div className="container">
           <div className="header-wrapper">
             <h1 className="heading">
-            <span className="elastic">Elastic</span> <br /><span
-              className="continfra-text"
-              >Container <strong>Infrastructure</strong> <br /></span
-            ><span className="blue">in Minutes</span>
+              <span className="elastic">Elastic</span> <br />
+              <span className="continfra-text">
+                Container <strong>Infrastructure</strong> <br />
+              </span>
+              <span className="blue">in Minutes</span>
             </h1>
             <div className="hero-p-wrapper">
               <p className="body-paragraph">
-                We&apos;re testing CLOUDMAGIC, a service that lets you deploy{" "}
+                We're testing CLOUDMAGIC, a service that lets you deploy{" "}
                 <em>autoscale container infrastructure</em> to your favorite
                 cloud – in minutes. <br />
                 <br />
-                <span className="sign-up-text">Sign up for early access.</span>
+                <span className="sign-up-text">
+                  Sign up for early access.
+                </span>
               </p>
             </div>
             <div className="hero-button-wrapper">
-              <a className="button" onClick={handleJoinWaitlist}>
+              <a
+                className="button"
+                onClick={handleJoinWaitlist}
+                style={{ color: "#fff" }}
+              >
                 Join Waitlist
               </a>
             </div>
@@ -61,17 +94,28 @@ export default function Home() {
       </div>
       <div className="bottom">
         <div className="bg-wrapper"></div>
-        <div className="container">
+        <div className="container auto">
           <div className="video-wrapper">
-            <a href="#" className="w-inline-block w-lightbox">
-              <Image
-                src={playbutton}
-                alt=""
-                className="play-image"
-                width={80}
-                height={80}
-              />
-            </a>
+            {showVideo ? (
+              <iframe
+                style={{width:"100%", height:"100%"}}
+                src={videoDetails.src}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            ) : isLoading ? (
+              <Spinner />
+            ) : (
+              <a href="#" className="w-inline-block w-lightbox" onClick={handlePlayClick}>
+                <Image
+                  src={playbutton}
+                  alt=""
+                  loading="lazy"
+                  className="play-image"
+                />
+              </a>
+            )}
           </div>
         </div>
       </div>
@@ -80,43 +124,21 @@ export default function Home() {
           <div className="client-wrapper">
             <div className="text-block">Planned support for</div>
             <div className="w-layout-grid client-image-grid">
-              <div
-                id="w-node-_782e7c2e-0d6a-f1db-6204-e5cb059b2a64-94e90351"
-                className="image-wrap"
-              >
-                <Image
-                  src={azure}
-                  alt="Azure Logo"
-                  className="client-image"
-                  width={84}
-                  height={84}
-                />
-              </div>
-              <div
-                id="w-node-_43a2f9a2-cbd3-1de7-dab9-493cade4a36c-94e90351"
-                className="image-wrap"
-              >
-                <Image
-                  src={amazonicon}
-                  alt="Amazon Logo"
-                  className="client-image"
-                  width={88}
-                  height={88}
-                />
-              </div>
-              <div
-                id="w-node-_43a2f9a2-cbd3-1de7-dab9-493cade4a36c-94e90351"
-                className="image-wrap"
-              >
-                <Image
-                  src={googleimg}
-                  alt="Amazon Logo"
-                  className="client-image"
-                  width={84}
-                  height={84}
-
-                />
-              </div>
+              {[
+                { src: azure, alt: "Azure Logo" },
+                { src: amazonicon, alt: "Amazon Logo" },
+                { src: googleimg, alt: "Google Logo" },
+              ].map((client, index) => (
+                <div key={index} className="image-wrap">
+                  <Image
+                    src={client.src}
+                    alt={client.alt}
+                    className="client-image"
+                    width={84}
+                    height={84}
+                  />
+                </div>
+              ))}
             </div>
           </div>
         </div>
@@ -161,7 +183,6 @@ export default function Home() {
                   </Link>
                 </div>
               </div>
-
               <div
                 id="w-node-_6133e129-69fc-2ce6-1b85-5cc610993ee1-10993ebe"
                 className="navigation-block center"
